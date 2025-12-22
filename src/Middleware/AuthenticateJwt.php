c5180d86ecb0b602cdbb19d7da25ca277808d3fd
Stephen Luck Enguio <stephenenguio1120@gmail.com>
2025-12-22 13:56:48 +0800

    return user role

--- src/Middleware/AuthenticateJwt.php
+++ src/Middleware/AuthenticateJwt.php
@@ -40,7 +40,7 @@ class AuthenticateJwt
 
             $request->setUserResolver(fn () => $user);
 
-            $request->merge(['user_id' =>  $user->id, 'token' => $token]);
+            $request->merge(['user_id' =>  $user->id, 'token' => $token, 'user_role' => $user->role]);
 
         } catch (\Exception $e) {
             return response()->json([
